e757a07441dc06280874f9b4aa5b839176a39b39
lumbricina <55181574+lumbricina@users.noreply.github.com>
2021-12-07 15:50:18 +0700

    Update proposal.php

--- app/views/fit/proposal.php
+++ app/views/fit/proposal.php
@@ -17,24 +17,63 @@
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
+                                            <th>Tanggal</th>
+                                            <th>Nama Mahasiswa</th>
+                                            <th>Judul</th>
                                             <th>Pembimbing 1</th>
                                             <th>Pembimbing 2</th>
-                                            <th>Judul</th>
+                                            <th>Ringkasan</th>
                                             <th>Status</th>
+                                            <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td>Lala</td>
-                                            <td>Nina</td>
-                                            <td>Aplikasi something</td>
+                                            <td>01/01/2022</td>
+                                            <td>Mint Chocolate</td>
+                                            <td>Pengembangan cokelat berbasis mint</td>
+                                            <td>KG</td>
+                                            <td>HG</td>
+                                            <td>ini ringkasan</td>
                                             <td>acc/tolak</td>
-                                            </tr>
+                                            <td>
+                                                <div class="row">
+                                                <div class="dropdown no-arrow ml-2">
+                                                    <button class="btn btn-warning btn-circle btn-sm dropdown-toggle" type="button"
+                                                    id="dropdownMenuButton" data-toggle="modal" aria-haspopup="true" data-target="#poprevisi"
+                                                    aria-expanded="false" title="Revisi">
+                                                    <i class="fas fa-exclamation-triangle"></i>
+                                                    </button>
+                                                    <div class="modal fade" id="poprevisi" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                        <div class="modal-dialog" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel">Revisi</h5>
+                                                                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">×</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">Masukkan feedback / revisi</div>
+                                                                <div class="form-group px-3 pb-3">
+                                                                <input type="text" class="form-control form-control-user" id="text" aria-describedby="text" placeholder="masukan isi revisi" autocomplete="off">
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                                                    <a class="btn btn-primary" href="#">Submit</a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <a class="btn btn-success btn-circle btn-sm ml-2" href="#" title="Setujui"><i class="fas fa-check"></i></a>
+                                            </div></div>
+                                            </td>
+                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
-                    </div>                           
+                    </div>                   
 
                 <!-- /.container-fluid -->
 
