73b2309f1f85d262eab45f549b0be80425b86a4c
Milenia Ulwan Zafira <mileniaulwanzafira@gmail.com>
2021-12-09 00:09:48 +0700

    Update logout.php

--- app/views/fit/logout.php
+++ app/views/fit/logout.php
@@ -1,19 +1,9 @@
 <?php
-
+ 
 session_start();
-
-if (isset($_SESSION['user'])) {
-    $_SESSION = array();
-
-    if (isset($_COOKIE[session_name()])) {
-        setcookie(session_name(), '', time() - 3600);
-    }
-
-    session_destroy();
-}
-
-setcookie('user', '', time() - 3600);
-
-header('location:login');
-
+session_destroy();
+ 
+header("Location: login");
+ 
+?>
 ?>
