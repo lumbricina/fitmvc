bb17874c028a9a1341b2fd3e756812de1a748f5c
lumbricina <55181574+lumbricina@users.noreply.github.com>
2021-12-09 16:32:54 +0700

    Update uppropo.php

--- app/views/fit/uppropo.php
+++ app/views/fit/uppropo.php
@@ -10,7 +10,7 @@ if(isset($_POST['pem1']))
     $judul = $_POST['judul'];
     $ringkasan = $_POST['ringkasan'];
     $date = gmdate('Y-m-d');
-    $timezone  = +7;
+    $timezone  = +7; //WIB
     $time = gmdate('H:i:s', time() + 3600*($timezone+date("I")));
 
     $prop="INSERT INTO proposal (id, date, time, nama, pembimbing1, pembimbing2, judul, ringkasan) VALUES (NULL, '$date', '$time', '$mahasiswa','$pem1','$pem2','$judul','$ringkasan')";
