037882c1bd12e3703c21ccb92c992e9380e43dd4
lumbricina <55181574+lumbricina@users.noreply.github.com>
2021-12-09 16:32:08 +0700

    Update uppropo.php

--- app/views/fit/uppropo.php
+++ app/views/fit/uppropo.php
@@ -9,8 +9,11 @@ if(isset($_POST['pem1']))
     $pem2 = $_POST['pem2'];
     $judul = $_POST['judul'];
     $ringkasan = $_POST['ringkasan'];
+    $date = gmdate('Y-m-d');
+    $timezone  = +7;
+    $time = gmdate('H:i:s', time() + 3600*($timezone+date("I")));
 
-    $prop="INSERT INTO proposal (id, date, time, nama, pembimbing1, pembimbing2, judul, ringkasan) VALUES (NULL, date, time, '$mahasiswa','$pem1','$pem2','$judul','$ringkasan')";
+    $prop="INSERT INTO proposal (id, date, time, nama, pembimbing1, pembimbing2, judul, ringkasan) VALUES (NULL, '$date', '$time', '$mahasiswa','$pem1','$pem2','$judul','$ringkasan')";
 
     if(mysqli_query($conn, $prop)){
         echo "uda";
