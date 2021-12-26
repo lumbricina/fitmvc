f04f537b8de1be1ea667a1be24512c764a063024
lumbricina <55181574+lumbricina@users.noreply.github.com>
2021-12-27 01:20:20 +0700

    Update delsidang.php

--- app/views/fit/delsidang.php
+++ app/views/fit/delsidang.php
@@ -2,23 +2,18 @@
 $data['page_title'] = "DelJadwalSidang";$this->view("fit/headeradmin", $data);
 include("koneksi.php");
 
+$id = $_GET['id'];
+$date = $_GET['date'];
+$time = $_GET['time'];
+$mahasiswa = $_GET['mahasiswa'];
+
+
    if(isset($_POST['submit']))
-   {   $id = $_POST['id'];
-       $date = $_POST['tanggal'];
-       $time = $_POST['waktu'];
-       $isi = $_POST['mahasiswa'];
-   
-       $deletesidang="DELETE FROM jadwalsidang WHERE jadwalsidang.id = '$id'";
-   
-       if(mysqli_query($conn, $deletesidang)){
-           header('location:jadwalsidang');
-       }else{
-           echo "eror: " . mysqli_error($conn);
-           header('Refresh : 3, location:jadwalsidang');
-       }
+   {   
+       $deletesidang="DELETE FROM jadwalsidang WHERE id='$id' ";
+       mysqli_query($conn, $deletesidang);
    }
    
-$query="SELECT * from jadwalsidang"
 
 
 ?>
@@ -29,7 +24,7 @@ $query="SELECT * from jadwalsidang"
 
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Persetujuan Proposal</h1>                      
+    <h1 class="h3 mb-0 text-gray-800">Delete Jadwal</h1>                      
 </div>
 
 <!-- Content Row -->
@@ -51,17 +46,14 @@ $query="SELECT * from jadwalsidang"
                         <div class="col-12">
                         <p> Untuk batal tekan tombol back (kembali ke halaman sebelumnya) </p>
                             <form method="POST" action="">
+                            <h7 class="text-gray-900 mb-4">Tanggal</h7>
+                                <input type="date" readonly class="form-control form-control-user" name="tanggal" class="txtField" value="<?php echo $date; ?>">
+                                    <br>
+                            <h7 class="text-gray-900 mb-4">Waktu</h7>
+                                <input type="time" readonly class="form-control form-control-user" name="waktu" class="txtField" value="<?php echo $time; ?>">
+                                    <br>
                             <h7 class="text-gray-900 mb-4">Mahasiswa</h7>
-                                <input type="text" readonly class="form-control form-control-user" name="nama"  value="<?php echo $row['id']; ?>">
-                                    <br>
-                            <h7 class="text-gray-900 mb-4">Pembimbing1</h7>
-                                <input type="text" readonly class="form-control form-control-user" name="pembimbing1" class="txtField" value="<?php echo $row['tanggal']; ?>">
-                                    <br>
-                            <h7 class="text-gray-900 mb-4">Pembimbing2</h7>
-                                <input type="text" readonly class="form-control form-control-user" name="pembimbing2" class="txtField" value="<?php echo $row['waktu']; ?>">
-                                    <br>
-                            <h7 class="text-gray-900 mb-4">Judul</h7>
-                                <input type="text" readonly class="form-control form-control-user" name="judul" class="txtField" value="<?php echo $row['mahasiswa']; ?>">
+                                <input type="text" readonly class="form-control form-control-user" name="mahasiswa" class="txtField" value="<?php echo $mahasiswa; ?>">
                                     <br>
                                 <input type="submit" class="btn btn-primary btn-user btn-block" name="submit" value="Delete" class="buttom">
                             </form>
