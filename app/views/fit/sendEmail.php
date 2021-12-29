ec63d35ea54d82b0564924352cd4cc3d47271c3a
lumbricina <55181574+lumbricina@users.noreply.github.com>
2021-12-30 03:11:27 +0700

    Update sendEmail.php

--- app/views/fit/sendEmail.php
+++ app/views/fit/sendEmail.php
@@ -1,12 +1,15 @@
 <?php 
 
 if (isset($_POST['sending_email_btn'])) {
-$to = "mileniaulwanzafira@example.com";
+$to = "mileniaulwanzafira@gmail.com" . ',';
+$to .= 'aidan@example.com' . ', '; // note the comma
+$to .= 'wez@example.com';
 $subject = "My subject";
 $txt = "Hello world!";
 
+$headers = "From: fit.it.its@gmail.com";
 // Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 mail($to,$subject,$txt,$headers);
